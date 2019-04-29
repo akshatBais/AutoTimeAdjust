d0d5d6d9895b2ea03b40c1948b630add76a42b45
akshatBais <akshatsinghbais@gmail.com>
2019-04-29 15:09:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zs9aph
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zs9aph)
---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component,OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators,FormArray} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  timeCalcuate:FormGroup;
+
+  ngOnInit(){
+    this.timeCalcuate = new FormGroup({
+      Hours : new FormControl(),
+      Minutes : new FormControl(),
+      Seconds : new FormControl()
+    });
+  }
+
+  checkTime(Hours , Minutes , Seconds) {
+    console.log(Hours , + " " + Minutes )
+    let newSecondsValue = Seconds % 60;
+    let totalMinutes = Number(Minutes) + ((Seconds - (Seconds % 60)) / 60);
+    let newMinutesValue = totalMinutes % 60;
+    let newHourValue = Number(Hours) + ((totalMinutes - (totalMinutes % 60)) / 60);
+
+    this.timeCalcuate.get('Hours').setValue(newHourValue);
+    this.timeCalcuate.get('Minutes').setValue(newMinutesValue);
+    this.timeCalcuate.get('Seconds').setValue(newSecondsValue);
+  }
+
+ 
+ 
+
+ 
+}
+
+
+// angular form is group of controls
